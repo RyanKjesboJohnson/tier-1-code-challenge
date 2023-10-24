592b790b1c35f911c790ebeddd51b3473a451add
Ryan Kjesbo-Johnson <Ryanmark86@gmail.com>
2023-10-24 07:53:34 -0500

    completed assignment but some tests still not passing

--- script.js
+++ script.js
@@ -36,16 +36,37 @@ myScrabbleTiles.push({ tile: 'F', score : 4 });
 console.log(myScrabbleTiles);
 
 // 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop to add up and return the sum of all the scores for a given array of scrabble tiles.
+let exampleTiles = ['N','K','K','Z','F','Z'];
+console.log(exampleTiles);
+let totalScore = 0;
+
+
 function calculateScore(tiles) {
+  for (let t of tiles){
+   for (let scrabbleTile of myScrabbleTiles){
+    if (scrabbleTile.tile === t){
+      totalScore += scrabbleTile.score;
+      console.log("looking at", t, "and found score of", scrabbleTile.score);
+    }
+   }
+  };
+  return totalScore;
 
 }
 
+console.log(calculateScore(exampleTiles));
+
+
 
 // 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
-
+let myScore = totalScore;
+console.log(myScore);
 
 // 8. Check whether or not your score is higher than the highScore. If your score is higher, change highScore to the new high score.
-
+if (highScore < totalScore){
+  highScore = totalScore
+}
+console.log("the new high score is:", highScore);
 
 // DONE!
 
